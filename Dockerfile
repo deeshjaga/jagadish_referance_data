d1342a3922ed2faf3ffdff1bf59eca9d5b5aeb7d
Anoop Nerallapally <NerallapallyA@b2b.regn.net>
2024-02-21 16:47:33 -0500

    updated code to use firefox and also run it in docker

--- Dockerfile
+++ Dockerfile
@@ -1,19 +1,36 @@
-FROM python:3.10
+FROM debian:buster-slim
 
-ARG CHROME_VERSION="118.0.5993.70-1"
-RUN wget -O /tmp/chrome.deb https://dl.google.com/linux/chrome/deb/pool/main/g/google-chrome-stable/google-chrome-stable_${CHROME_VERSION}_amd64.deb
-RUN apt-get -y update
-RUN apt-get install -y /tmp/chrome.deb
-#
-RUN apt-get install -yqq unzip
-RUN wget --no-check-certificate -O /tmp/chromedriver.zip https://edgedl.me.gvt1.com/edgedl/chrome/chrome-for-testing/118.0.5993.70/linux64/chromedriver-linux64.zip
-RUN unzip /tmp/chromedriver.zip -d /tmp/
-RUN cp /tmp/chromedriver-linux64/chromedriver /usr/local/bin/
+ENV DEBIAN_FRONTEND=noninteractive
 
-RUN apt-get install -y vim
+RUN apt-get update && \
+    apt-get install -y --no-install-recommends python3 python3-pip && \
+    apt-get clean && \
+    rm -rf /var/lib/apt/lists/*
 
-RUN pip install --upgrade pip
-RUN pip install selenium boto3
+RUN apt-get update && \
+    apt-get install -y --no-install-recommends firefox-esr && \
+    apt-get clean && \
+    rm -rf /var/lib/apt/lists/*
+
+
+RUN mkdir -p /downloads
+ENV DOWNLOAD_PATH /downloads
+
+RUN pip3 install selenium==3.141.0
+RUN pip3 install --upgrade urllib3==1.26.16
+RUN pip3 install boto3
+
+
+RUN apt-get update && \
+    apt-get install -y wget && \
+    wget https://github.com/mozilla/geckodriver/releases/download/v0.34.0/geckodriver-v0.34.0-linux64.tar.gz && \
+    tar -xvzf geckodriver-v0.34.0-linux64.tar.gz && \
+    chmod +x geckodriver && \
+    mv geckodriver /usr/local/bin/ && \
+    rm geckodriver-v0.34.0-linux64.tar.gz && \
+    apt-get purge -y wget && \
+    apt-get autoremove -y
+
 
 WORKDIR /scratch/BTS_Referance_Data/
 COPY bts_reference_data_scraper.py .
@@ -22,55 +39,3 @@ COPY bts_reference_data_scraper.py .
 CMD ["python", "./bts_reference_data_scraper.py"]
 ENTRYPOINT ["tail", "-f", "/dev/null"]
 
-#RUN wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add -
-#RUN sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
-##
-#RUN apt-get -y update
-#RUN apt-get install -y google-chrome-stable
-##
-#RUN apt-get install -yqq unzip
-#RUN wget --no-check-certificate -O /tmp/chromedriver.zip https://edgedl.me.gvt1.com/edgedl/chrome/chrome-for-testing/118.0.5993.70/linux64/chromedriver-linux64.zip
-#RUN unzip /tmp/chromedriver.zip -d /tmp/
-#RUN cp /tmp/chromedriver-linux64/chromedriver /usr/bin/chromedriver
-
-#RUN echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" | \
-#    tee -a /etc/apt/sources.list.d/google.list && \
-#    wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | \
-#    apt-key add - && \
-#    apt-get update && \
-#    apt-get install -y google-chrome-stable libxss1
-#
-#RUN BROWSER_MAJOR=$(google-chrome --version | sed 's/Google Chrome \([0-9]*\).*/\1/g') && \
-#    wget https://chromedriver.storage.googleapis.com/LATEST_RELEASE_${BROWSER_MAJOR} -O chrome_version && \
-#    wget https://chromedriver.storage.googleapis.com/`cat chrome_version`/chromedriver_linux64.zip && \
-#    unzip chromedriver_linux64.zip && \
-#    mv chromedriver /usr/local/bin/ && \
-#    DRIVER_MAJOR=$(chromedriver --version | sed 's/ChromeDriver \([0-9]*\).*/\1/g') && \
-#    echo "chrome version: $BROWSER_MAJOR" && \
-#    echo "chromedriver version: $DRIVER_MAJOR" && \
-#    if [ $BROWSER_MAJOR != $DRIVER_MAJOR ]; then echo "VERSION MISMATCH"; exit 1; fi
-#RUN apt-get install -yqq unzip
-#RUN wget --no-check-certificate -O /tmp/chromedriver.zip https://edgedl.me.gvt1.com/edgedl/chrome/chrome-for-testing/118.0.5993.70/linux64/chromedriver-linux64.zip
-#RUN unzip /tmp/chromedriver.zip -d /tmp/
-#RUN cp /tmp/chromedriver-linux64/chromedriver /usr/local/bin/
-
-
-
-#WORKDIR /tmp/
-#RUN apt update
-#RUN wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-#RUN apt install ./google-chrome-stable_current_amd64.deb
-#
-#RUN wget https://edgedl.me.gvt1.com/edgedl/chrome/chrome-for-testing/118.0.5993.70/linux64/chromedriver-linux64.zip
-#RUN unzip chromedriver-linux64.zip
-#RUN mv /tmp/chromedriver-linux64/chromedriver /usr/bin/chromedriver
-
-#RUN useradd -ms /bin/bash newuser
-#USER newuser
-
-#WORKDIR /scratch/BTS_Referance_Data/
-#COPY bts_reference_data_scraper.py .
-
-
-#CMD ["python", "./bts_reference_data_scraper.py"]
-#ENTRYPOINT ["tail", "-f", "/dev/null"]
--- bts_reference_data_scraper.py
+++ bts_reference_data_scraper.py
@@ -1,8 +1,10 @@
-import boto3
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.firefox.firefox_profile import FirefoxProfile
+import os
+import time
+import boto3
 import zipfile
 import os
 import re
@@ -12,7 +14,7 @@ from datetime import date
 today = date.today()
 print("today's date is {}".format(today))
 
-# S3 account information
+# # S3 account information
 # sts_client = boto3.client('sts')
 # cdp_role_arn = 'arn:aws:iam::537054356694:role/consumer-prod-cdpe-fm-traffic'
 # cdp_session_name = 'cdp_consumer_datasynthesis'
@@ -28,56 +30,70 @@ print("today's date is {}".format(today))
 # )
 # aes_s3 = aes_tickets_data_engineering.client("s3")
 
+# S3 account information test
+sts_client = boto3.client('sts')
+cdp_role_arn = 'arn:aws:iam::287482246495:saml-provider/ADFS'
+cdp_session_name = 'cdp_consumer_datasynthesis'
+assumed_role = sts_client.assume_role(
+    RoleArn=cdp_role_arn,
+    RoleSessionName=cdp_session_name
+)
+
+aes_tickets_data_engineering = boto3.Session(
+    aws_access_key_id=assumed_role['Credentials']['AccessKeyId'],
+    aws_secret_access_key=assumed_role['Credentials']['SecretAccessKey'],
+    aws_session_token=assumed_role['Credentials']['SessionToken']
+)
+aes_s3 = aes_tickets_data_engineering.client("s3")
+
+# # # for local testing of the program and to connect to s3 buckets
+# aes_tickets_data_engineering = boto3.Session(profile_name="fsadmin-ticketsengineering")
+# aes_s3 = boto3.client("s3")
+
 # Specify the download directory path
 download_directory = "/scratch/BTS_Referance_Data/"
-# download_directory = "/Users/nerallapallya/PycharmProjects/BTS_reference_data"
-# Configure Chrome options to set the download directory
-chrome_options = webdriver.ChromeOptions()
-chrome_options.add_experimental_option('prefs', {
-    'download.default_directory': download_directory
-})
-chrome_options.add_argument('--no-sandbox')
-chrome_options.add_argument('--headless')
-chrome_options.add_argument('--ignore-certificate-errors')
-chrome_options.add_argument('--disable-dev-shm-usage')
-chrome_options.add_argument('--disable-extensions')
-chrome_options.add_argument('--disable-gpu')
-chrome_options.binary_location = '/usr/bin/google-chrome'
+
+profile = FirefoxProfile()
+profile.set_preference("browser.download.dir", download_directory)
+profile.set_preference("browser.download.folderList", 2)
+profile.set_preference("browser.helperApps.neverAsk.saveToDisk", "application/octet-stream")
+
+# Set up Firefox options with the custom profile
+firefox_options = Options()
+firefox_options.headless = True
+firefox_options.profile = profile
+geckodriver_path = '/usr/local/bin/geckodriver'
 
 
 # The SELENIUM_SERVER_URL environment variable must be set in your Docker container
 carrier_url = "https://www.transtats.bts.gov/DL_SelectFields.aspx?gnoyr_VQ=GDH&QO_fu146_anzr=N8vn6v10%20f722146%20gnoyr5"
-carrier_browser = webdriver.Chrome(options=chrome_options)
+carrier_browser = webdriver.Firefox(executable_path=geckodriver_path, options=firefox_options)
 carrier_browser.get(carrier_url)
-# carrier_pattern = r'^T_CARRIER_DECODE_*\.zip$'
-carrier_pattern = 'DL_SelectFields.zip'
+carrier_pattern = 'T_CARRIER_DECODE'
 carrier_target = None
 # response = requests.get(main_url)
 
 world_area_code_url = "https://www.transtats.bts.gov/DL_SelectFields.aspx?gnoyr_VQ=GEI&QO_fu146_anzr=N8vn6v10%20f722146%20gnoyr5"
-wac_browser = webdriver.Chrome(options=chrome_options)
+wac_browser = webdriver.Firefox(executable_path=geckodriver_path, options=firefox_options)
 wac_browser.get(world_area_code_url)
-# wac_patter = r'^T_WAC_COUNTRY_STATE_*\.zip$'
-wac_pattern = 'DL_SelectFields.zip'
+wac_pattern = 'T_WAC_COUNTRY_STATE'
 wac_target = None
 
 aircraft_type_url = "https://www.transtats.bts.gov/DL_SelectFields.aspx?gnoyr_VQ=GDD&QO_fu146_anzr=N8vn6v10%20f722146%20gnoyr5"
-at_browser = webdriver.Chrome(options=chrome_options)
+at_browser = webdriver.Firefox(executable_path=geckodriver_path, options=firefox_options)
 at_browser.get(aircraft_type_url)
-# wac_patter = r'^T_WAC_COUNTRY_STATE_*\.zip$'
-at_pattern = 'DL_SelectFields.zip'
+at_pattern = 'T_AIRCRAFT_TYPES'
 at_target = None
 
 master_coordinates_url = "https://www.transtats.bts.gov/DL_SelectFields.aspx?gnoyr_VQ=FLL&QO_fu146_anzr=N8vn6v10%20f722146%20gnoyr5"
-mc_browser = webdriver.Chrome(options=chrome_options)
+mc_browser = webdriver.Firefox(executable_path=geckodriver_path, options=firefox_options)
 mc_browser.get(master_coordinates_url)
-# wac_patter = r'^T_WAC_COUNTRY_STATE_*\.zip$'
-mc_pattern = 'DL_SelectFields.zip'
+mc_pattern = 'T_MASTER_CORD'
 mc_target = None
 
 # upload S3 loacation
-# bucket_name = "squad-reference-data-ops-restricted-cdp-us-east-1"
-bucket_name = "squad-reference-data-ops-restricted-cdp-us-east-1"
+bucket_name = "cp-de-nerallapallya-test"
+#bucket_name = "squad-reference-data-ops-restricted-cdp-us-east-1"
 object_key1 = "Carrier_decode/{}".format(today) + "/"
 object_key2 = "World_area_codes/{}".format(today) + "/"
 object_key3 = "Aircraft_type/{}".format(today) + "/"
@@ -87,7 +103,6 @@ carrier_file = download_directory + "T_CARRIER_DECODE.csv"
 wac_file = download_directory + "T_WAC_COUNTRY_STATE.csv"
 at_file = download_directory + "T_AIRCRAFT_TYPES.csv"
 mc_file = download_directory + "T_MASTER_CORD.csv"
-
 
 # download carrier_decode file
 
@@ -223,31 +238,37 @@ if mc_target:
 
 time.sleep(10)
 
-# # upload world area code file
-# aes_s3.upload_file(
-#     Filename=download_directory + "/{}".format(wac_file),
-#     Bucket=bucket_name,
-#     Key=object_key1
-# )
-#
-# # upload carrier decode file
-# aes_s3.upload_file(
-#     Filename=download_directory + "/{}".format(carrier_file),
-#     Bucket=bucket_name,
-#     Key=object_key2
-# )
-#
-# # upload aircraft_type file
-# aes_s3.upload_file(
-#     Filename=download_directory + "/{}".format(at_file),
-#     Bucket=bucket_name,
-#     Key=object_key3
-# )
-#
-# # upload master_coordinate file
-# aes_s3.upload_file(
-#     Filename=download_directory + "/{}".format(mc_file),
-#     Bucket=bucket_name,
-#     Key=object_key4
-# )
+
+downloaded_files = os.listdir(download_directory)
+for file in downloaded_files:
+    if file.endswith(".pdf"):  # Example: Move only PDF files
+        os.rename(os.path.join(download_directory, file), os.path.join("/path/to/new/directory", file))
+
+
+# upload world area code file
+aes_s3.upload_file(
+    Filename=download_directory + "/{}".format(wac_file),
+    Bucket=bucket_name,
+    Key=object_key1
+)
+# upload carrier decode file
+aes_s3.upload_file(
+    Filename=download_directory + "/{}".format(carrier_file),
+    Bucket=bucket_name,
+    Key=object_key2
+)
+
+# upload aircraft_type file
+aes_s3.upload_file(
+    Filename=download_directory + "/{}".format(at_file),
+    Bucket=bucket_name,
+    Key=object_key3
+)
+
+# upload master_coordinate file
+aes_s3.upload_file(
+    Filename=download_directory + "/{}".format(mc_file),
+    Bucket=bucket_name,
+    Key=object_key4
+)
 
